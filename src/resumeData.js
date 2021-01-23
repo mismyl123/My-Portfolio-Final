b134a38bfb0e3fc0c243168b5ed4af07e73f17f9
mismyl123 <mlalonde309@gmail.com>
2021-01-23 01:40:56 -0500

    commit

---
+++ src/resumeData.js
@@ -0,0 +1,139 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/mismyl123",
+    "name": "Michelle",
+    "role": "Full Stack Developer",
+    "linkedinId":"Your ID",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I like to learn and create applications in various parts of software development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+            "name":"linkedin",
+            "url":"https://www.linkedin.com/in/michelle-lalonde/",
+            "className":"fa fa-linkedin"
+
+        },
+        {
+            "name":"github",
+            "url":"https://github.com/mismyl123",
+            "className":"fa fa-github"
+        },
+        {
+            "name":"email",
+            "url":"mailto:mlalonde309@gmail.com",
+            "className":"fa fa-envelope"
+
+        }
+    ],
+
+    "aboutme":"I am currently a Masters student at The Hochschule Fulda and pursuing my masters in Global software Development from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+
+
+
+"address":"Florida, USA",
+"website":"https://github.com/mismyl123",
+"education":[
+    {
+        "UniversityName":"Valencia Community College",
+        "specialization":"Information Technology",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2007",
+        "Achievements":"Associates Degree"
+    },
+    {
+        "UniversityName":"Thinkful",
+        "specialization":"Engineering Immersion",
+        "MonthOfPassing":"February",
+        "YearOfPassing":"2021",
+        "Achievements":"Certificate"
+    }
+],
+"work":[
+    {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+    },
+    {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+    }
+],
+"skillsDescription":"",
+"skills":[
+    {
+        "skillname":"HTML5"
+    },
+    {
+        "skillname":"CSS"
+    },
+    {
+        "skillname":"Javascript"
+    },
+    {
+        "skillname":"JQuery"
+    },
+    {
+        "skillname":"React"
+    },
+    {
+        "skillname":"Node"
+    },
+    {
+        "skillname":"Postgres"
+    }
+],
+"portfolio":[
+    {
+        "id":1,
+        "name":"Gaming Social Network",
+        "imgurl":"images/portfolio/gsn.jpg",
+           
+        
+
+},
+{
+    "id":2,
+    "name":"Bookmarks Most Wanted",
+    "demo":"mobileapp",
+    "imgurl":"images/portfolio/bookmarks.jpg",
+    "Github":""
+
+},
+{
+    "id":3,
+    "name":"Petful",
+    "demo":"https://petful-client-one.vercel.app/",
+    "imgurl":"images/portfolio/petful.jpg",
+    "Github":"https://github.com/mismyl123/Petful-Client"
+
+},
+{
+    "id":4,
+    "name":"Spaced Repetition",
+    "demo":"https://spaced-repetition-navy.vercel.app/",
+    "imgurl":"images/portfolio/SR.jpg",
+    "Github":"https://github.com/mismyl123/spaced-repetition"
+
+}
+],
+"testimonials":[
+    {
+        "description":"Michelle is a great collaborator when we code!",
+        "name":"Angelo- co-worker"
+    },
+    {
+        "description":"Ive had the pleasure of working with Michelle on a development team for up to a year, she is driven and exudes problem solving and critical thinking skills; she’s been a great asset to our team.",
+        "name":"Marcus- co-worker"
+    },
+    {
+        "description":"We live in a society.",
+        "name":"Jack Napier"
+    }
+]
+    }
+    export default resumeData
